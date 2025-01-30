1f9e9863ea8d732653bcf131b02347030c74b61e
David Swatzell <thatsstrikethree@gmail.com>
2025-01-30 14:37:43 -0600

    update app/js

--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("http://localhost:3000/books")
-        .then(response => response.json())
-        .then(books => {
-            var container = document.getElementById("books");
-            books.forEach(book => {
-                var div = document.createElement("div");
-                div.innerHTML = "<h3>" + book.title + "</h3><p>" + book.author + "</p><p>$" + book.price + "</p>";
-                container.appendChild(div);
+    var booksContainer = document.getElementById("books");
+
+    function fetchBooks() {
+        fetch("http://localhost:3000/books")
+            .then(response => response.json())
+            .then(books => {
+                booksContainer.innerHTML = "";
+                books.forEach(function (book) {
+                    var bookDiv = document.createElement("div");
+                    bookDiv.innerHTML = `
+                        <h3>${book.title}</h3>
+                        <p>Author: ${book.author}</p>
+                        <p>Genre: ${book.genre}</p>
+                        <p>Price: $${book.price}</p>
+                        <button onclick="addToCart(${book.id}, ${book.price})">Add to Cart</button>
+                    `;
+                    booksContainer.appendChild(bookDiv);
+                });
             });
-        });
+    }
+
+    window.addToCart = function (bookId, price) {
+        alert("Book " + bookId + " added to cart!");
+    };
+
+    fetchBooks();
 });
