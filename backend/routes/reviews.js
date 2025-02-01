f302b883f5843867af23a24c1a905626d01a0a89
David Swatzell <thatsstrikethree@gmail.com>
2025-02-01 15:04:34 -0600

    add route for user specific reviews

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -8,4 +8,11 @@ router.get("/:bookId", function (req, res) {
     });
 });
 
+router.get("/user/:userId", function (req, res) {
+    db.all("SELECT * FROM reviews WHERE user_id = ?", [req.params.userId], function (err, rows) {
+        if (err) return res.status(500).send(err.message);
+        res.json(rows);
+    });
+});
+
 module.exports = router;
