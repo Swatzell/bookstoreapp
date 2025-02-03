386cf823c3b01a1b7774adddbd35f066e4b15978
David Swatzell <thatsstrikethree@gmail.com>
2025-02-03 10:25:52 -0600

    add some basic initial css, make reviews and login links on main page work correctly

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,16 +2,26 @@ var express = require("express");
 var db = require("../db");
 var router = express.Router();
 
+
 router.get("/", function (req, res) {
     db.all("SELECT * FROM books", function (err, rows) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
         res.json(rows);
     });
 });
 
 router.get("/reviews/:bookId", function (req, res) {
     db.all("SELECT * FROM reviews WHERE book_id = ?", [req.params.bookId], function (err, rows) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
         res.json(rows);
     });
 });
+
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 var express = require("express");
-var path = require("path");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
 var app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 
-var staticPath = path.join(__dirname, "../frontend");
-console.log("Serving static files from:", staticPath);
+app.use("/books", require("./routes/books"));
 
-
-app.use(express.static(staticPath));
-
+var path = require("path");
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(staticPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
-
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("Server running on port " + port);
+    console.log("Server running on port " + port);
 });
